a82599a9e6d3a51908b5210431a107ae83008711
Maxim <therikomero@gmail.com>
2024-12-27 04:07:14 +0700

    fix tests

--- src/test/java/org/example/fileservice/FileServiceApplicationTests.java
+++ src/test/java/org/example/fileservice/FileServiceApplicationTests.java
@@ -2,15 +2,10 @@ package org.example.fileservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.testcontainers.utility.TestcontainersConfiguration;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class FileServiceApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
--- src/test/java/org/example/fileservice/TestFileServiceApplication.java
+++ src/test/java/org/example/fileservice/TestFileServiceApplication.java
@@ -1,12 +1,12 @@
-package org.example.fileservice;
-
-import org.springframework.boot.SpringApplication;
-import org.testcontainers.utility.TestcontainersConfiguration;
-
-public class TestFileServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.from(FileServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
-}
+//package org.example.fileservice;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.testcontainers.utility.TestcontainersConfiguration;
+//
+//public class TestFileServiceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.from(FileServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+//	}
+//
+//}
